889c8ebb6d01059f6e4dc51a9abfa900fd901813
Joshua Pellerin <jpellerin@ibm.com@Joshuas-MacBook-Pro.local>
2021-06-27 18:40:28 -0400

    initial commit

---
+++ ImportProducts/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ImportProducts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Invalid Arguments");
+                return;
+            }
+
+            var command = args[0];
+            switch(command)
+            {
+                case "capterra":
+                    Console.WriteLine("importing capterra products...");
+                    break;
+                default:
+                    Console.WriteLine("Invalid command");
+                    break;
+            }
+            Console.WriteLine("Program exiting...");
+        }
+    }
+}
